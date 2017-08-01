25e0caa16bbb614597a4de836537084a16b28ca0
Cedric Staub <cs@squareup.com>
2017-08-01 16:39:25 -0700

    Tag version 1.1.0

--- certstrap.go
+++ certstrap.go
@@ -28,7 +28,7 @@ import (
 func main() {
 	app := cli.NewApp()
 	app.Name = "certstrap"
-	app.Version = "1.0.1"
+	app.Version = "1.1.0"
 	app.Usage = "A simple certificate manager written in Go, to bootstrap your own certificate authority and public key infrastructure."
 	app.Flags = []cli.Flag{
 		cli.StringFlag{
