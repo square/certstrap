63b33153a11df6b392243a6bfdafbe4d89521f06
Socheat Sok <alex@socheat.net>
2022-01-13 21:01:55 +0700

    Update `cmd/revoke_test.go` test

--- cmd/revoke_test.go
+++ cmd/revoke_test.go
@@ -75,7 +75,7 @@ func setupCA(t *testing.T, dt depot.Depot) {
 	}
 
 	// create certificate authority
-	caCert, err := pkix.CreateCertificateAuthority(key, caName, time.Now().Add(1*time.Minute), "", "", "", "", caName, nil)
+	caCert, err := pkix.CreateCertificateAuthority(key, caName, time.Now().Add(1*time.Minute), "", "", "", "", caName, nil, 0, false)
 	if err != nil {
 		t.Fatalf("could not create authority cert: %v", err)
 	}
