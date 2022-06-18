f021c621b65cff8473435571f4384d8f58a7e01b
Socheat Sok <alex@socheat.net>
2022-06-18 14:55:27 +0700

    Draft `sign` command to use `CreateIntermediateCertificateAuthorityWithOptions`

--- cmd/sign.go
+++ cmd/sign.go
@@ -145,7 +145,7 @@ func newSignAction(c *cli.Context) {
 	var crtOut *pkix.Certificate
 	if c.Bool("intermediate") {
 		fmt.Fprintln(os.Stderr, "Building intermediate")
-		crtOut, err = pkix.CreateIntermediateCertificateAuthority(crt, key, csr, expiresTime)
+		crtOut, err = pkix.CreateIntermediateCertificateAuthorityWithOptions(crt, key, csr, expiresTime, pkix.WithPathlenOption(c.Int("pathlen"), false))
 	} else {
 		if c.IsSet("path-length") {
 			fmt.Fprintln(os.Stderr, "The 'path-length' can only be used with 'intermediate' flag.")
