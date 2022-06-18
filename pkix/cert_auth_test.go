9f2013022d433581e938b1a8f4d73a7c4ca7719c
Socheat Sok <alex@socheat.net>
2022-06-18 18:49:02 +0700

    Add `CreateCertificateAuthorityWithOptions` test

--- pkix/cert_auth_test.go
+++ pkix/cert_auth_test.go
@@ -64,8 +64,84 @@ func TestCreateCertificateAuthority(t *testing.T) {
 	if crt.crt.PermittedDNSDomains[0] != ".example.com" {
 		t.Fatalf("Wrong permitted DNS domain, want %q, got %q", ".example.com", crt.crt.PermittedDNSDomains[0])
 	}
+}
 
-	if crt.crt.MaxPathLen != 0 {
-		t.Fatalf("Wrong MaxPathLen value, want %q, got %q", "0", crt.crt.MaxPathLen)
+func TestCreateCertificateAuthorityWithOptions(t *testing.T) {
+	tests := []struct {
+		name           string
+		pathlen        int
+		excludePathlen bool
+	}{{
+		name:           "pathlen: 0, excludePathlen: false",
+		pathlen:        0,
+		excludePathlen: false,
+	}, {
+		name:           "pathlen: 0, excludePathlen: false",
+		pathlen:        1,
+		excludePathlen: false,
+	}, {
+		name:           "pathlen: 0, excludePathlen: true",
+		pathlen:        0,
+		excludePathlen: true,
+	}, {
+		name:           "pathlen: 1, excludePathlen: true",
+		pathlen:        1,
+		excludePathlen: true,
+	}}
+
+	for _, tc := range tests {
+		t.Run(tc.name, func(t *testing.T) {
+			key, err := CreateRSAKey(rsaBits)
+			if err != nil {
+				t.Fatal("Failed creating rsa key:", err)
+			}
+
+			crt, err := CreateCertificateAuthorityWithOptions(key, "OU", time.Now().AddDate(5, 0, 0), "test", "US", "California", "San Francisco", "CA Name", []string{".example.com"}, WithPathlenOption(tc.pathlen, tc.excludePathlen))
+			if err != nil {
+				t.Fatal("Failed creating certificate authority:", err)
+			}
+			rawCrt, err := crt.GetRawCertificate()
+			if err != nil {
+				t.Fatal("Failed to get x509.Certificate:", err)
+			}
+
+			if err = rawCrt.CheckSignatureFrom(rawCrt); err != nil {
+				t.Fatal("Failed to check signature:", err)
+			}
+
+			if rawCrt.Subject.OrganizationalUnit[0] != "OU" {
+				t.Fatal("Failed to verify hostname:", err)
+			}
+
+			if !time.Now().After(rawCrt.NotBefore) {
+				t.Fatal("Failed to be after NotBefore")
+			}
+
+			if !time.Now().Before(rawCrt.NotAfter) {
+				t.Fatal("Failed to be before NotAfter")
+			}
+
+			if crt.crt.PermittedDNSDomainsCritical != true {
+				t.Fatal("Permitted DNS Domains is not set to critical")
+			}
+
+			if len(crt.crt.PermittedDNSDomains) != 1 {
+				t.Fatal("More than one entry found in list of permitted DNS domains")
+			}
+
+			if crt.crt.PermittedDNSDomains[0] != ".example.com" {
+				t.Fatalf("Wrong permitted DNS domain, want %q, got %q", ".example.com", crt.crt.PermittedDNSDomains[0])
+			}
+
+			if tc.excludePathlen {
+				if crt.crt.MaxPathLen != -1 {
+					t.Fatalf("Wrong MaxPathLen value, want %v, got %v", -1, crt.crt.MaxPathLen)
+				}
+			} else {
+				if crt.crt.MaxPathLen != tc.pathlen {
+					t.Fatalf("Wrong MaxPathLen value, want %v, got %v", tc.pathlen, crt.crt.MaxPathLen)
+				}
+			}
+		})
 	}
 }
